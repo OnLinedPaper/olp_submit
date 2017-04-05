8ed859b00ac75cb2f1ae339364951d579520aa10
Nicholas Reed <ndreed@g.clemson.edu>
2017-04-04 23:57:46 -0400

    added vars.py: now holds global variables and constants that used top reside in submit_v3.py

---
+++ vars.py
@@ -0,0 +1,29 @@
+
+delay = 4
+#if something takes longer than 4 seconds, something is up
+fieldnames = ['group_name', 'folder_name', 'folder_value']
+#these will be used by CSV files
+
+errorlog = './.sys/.errorlog.txt'
+errorrec = './.sys/.errorarchive.txt'
+savefile = './.sys/.save.txt'
+replacefile = './.sys/.replace.csv'
+askfile = './.sys/.ask.csv'
+serrorfile = './.sys/.submiterror.csv'
+slimitfile = './.sys/.submitlimit.csv'
+#store the names for easier use
+
+activefile = ''
+activegroup = ''
+
+submitfailures = 0
+submitlimits = 0
+submiterrors = 0
+submitasks = 0
+submitsuccesses = 0
+
+global drivr
+global modal_box
+global entry_field
+global check_button
+global klist
